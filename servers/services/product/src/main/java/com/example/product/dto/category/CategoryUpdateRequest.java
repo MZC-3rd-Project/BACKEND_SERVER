263f47e68a063135428f61c209545248a5de650f
ddingjoo <ddingsha9@teambind.co.kr>
2026-02-09 21:23:23 +0900

    feat: 카테고리 관리 CRUD + 트리 조회 API 구현 (#25)
    - Category 엔티티: 트리 구조 (parentId, depth, sortOrder) (#26)
    - CategoryRepository: 자식 조회, 트리 정렬 메서드 (#27)
    - CategoryService: 생성/수정/삭제/트리 조회 비즈니스 로직 (#28)
    - DTO: CategoryCreateRequest, CategoryUpdateRequest, CategoryResponse, CategoryTreeResponse (#29)
    - CategoryApi: Swagger 인터페이스 (#30)
    - CategoryController: REST API 구현 (#31)

---
+++ servers/services/product/src/main/java/com/example/product/dto/category/CategoryUpdateRequest.java
@@ -0,0 +1,17 @@
+package com.example.product.dto.category;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CategoryUpdateRequest {
+
+    @NotBlank(message = "카테고리 이름은 필수입니다")
+    @Size(max = 100, message = "카테고리 이름은 100자 이하여야 합니다")
+    private String name;
+
+    private int sortOrder;
+}
