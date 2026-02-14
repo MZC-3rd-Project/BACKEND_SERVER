4667b1776973e6e856b339fe26bcdc08f133698b
ddingjoo <ddingsha9@teambind.co.kr>
2026-02-14 19:20:27 +0900

    fix: add schema repair SQL and restore ShedLock LockProvider

--- libs/config/shedlock/src/main/java/com/example/config/shedlock/ShedLockAutoConfiguration.java
+++ libs/config/shedlock/src/main/java/com/example/config/shedlock/ShedLockAutoConfiguration.java
@@ -4,7 +4,6 @@ import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.redis.spring.RedisLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -17,7 +16,6 @@ public class ShedLockAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    @ConditionalOnBean(RedisConnectionFactory.class)
     public LockProvider lockProvider(RedisConnectionFactory connectionFactory) {
         return new RedisLockProvider(connectionFactory);
     }
