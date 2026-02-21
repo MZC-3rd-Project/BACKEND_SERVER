d42f8596480f5441c1e4a5ee7a07c6c90dc1a481
ddingjoo <ddingsha9@teambind.co.kr>
2026-02-21 22:43:26 +0900

    feat(media): add media url query reissue api

--- servers/services/media-api/src/main/java/com/example/media/repository/MediaLinkRepository.java
+++ servers/services/media-api/src/main/java/com/example/media/repository/MediaLinkRepository.java
@@ -22,4 +22,6 @@ public interface MediaLinkRepository extends JpaRepository<MediaLink, Long> {
             Long ownerId,
             MediaUsageType usageType
     );
+
+    Optional<MediaLink> findTopByMediaIdOrderByCreatedAtDesc(Long mediaId);
 }
