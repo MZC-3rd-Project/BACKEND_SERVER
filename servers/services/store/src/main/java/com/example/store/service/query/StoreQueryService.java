abdecb4d9288afe80e88bee315e7c61429290a80
waney <ssdii66@gmail.com>
2026-03-07 21:06:14 +0900

    feature(#839): store error storeList

--- servers/services/store/src/main/java/com/example/store/service/query/StoreQueryService.java
+++ servers/services/store/src/main/java/com/example/store/service/query/StoreQueryService.java
@@ -1,5 +1,9 @@
 package com.example.store.service.query;
 
+import com.example.core.exception.BusinessException;
+import com.example.core.exception.CommonErrorCode;
+import com.example.core.exception.ErrorCode;
+import com.example.core.exception.TechnicalException;
 import com.example.store.dto.response.StoreListResponse;
 import com.example.store.repository.StoresRepository;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +13,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import org.springframework.data.domain.Pageable;
+
+import static com.example.store.exception.StoreErrorCode.STORE_NOT_FOUND;
 
 
 @Slf4j
@@ -20,7 +26,13 @@ public class StoreQueryService {
     private final StoresRepository storesRepository;
 
     public Page<StoreListResponse>  getStoreListInfo(Pageable pageable){
-        return storesRepository.findStoreList(pageable);
+        try {
+            return storesRepository.findStoreList(pageable);
+        } catch (BusinessException e){
+            throw new BusinessException(STORE_NOT_FOUND);
+        } catch (TechnicalException e){
+            throw new TechnicalException(CommonErrorCode.INTERNAL_ERROR);
+        }
     }
 
 
