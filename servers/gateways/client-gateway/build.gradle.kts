b795ecce1ad24d4e312eb65ff04e16bf8e800895
ddingjoo <ddingsha9@teambind.co.kr>
2026-03-18 09:03:37 +0900

    feat: add gateway swagger ui

--- servers/gateways/client-gateway/build.gradle.kts
+++ servers/gateways/client-gateway/build.gradle.kts
@@ -6,9 +6,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.8.13")
 
     // Shared modules
     implementation(project(":libs:contracts:http"))
+    implementation(project(":libs:openapi:config")) {
+        exclude(group = "org.springdoc", module = "springdoc-openapi-starter-webmvc-ui")
+    }
     implementation(project(":libs:security:security-starter"))
 
     // Lombok
